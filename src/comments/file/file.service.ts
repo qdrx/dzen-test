c07a965aa5e95b5a3c731578d606d019b6fa8db5
qdrx <qdrx@github.com>
2024-08-28 16:47:31 +0000

    chore: add validation and resize

--- src/comments/file/file.service.ts
+++ src/comments/file/file.service.ts
@@ -2,13 +2,21 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import * as path from 'node:path';
 import * as fs from 'node:fs';
 import { customAlphabet } from 'nanoid';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class FileService {
   private readonly _filePath = path.join(__dirname, '../../..', 'uploads');
+  private readonly _allowedImageTypes = ['jpg', 'png', 'gif'];
 
   async uploadFile(file: Express.Multer.File) {
     const [_, fileExtention] = file.originalname.split('.');
+    if (this._allowedImageTypes.includes(fileExtention)) {
+      const { width, height } = await sharp(file.buffer).metadata();
+      if (width > 320 || height > 240) {
+        file.buffer = await sharp(file.buffer).resize(320, 240).toBuffer();
+      }
+    }
     const newFilename = await this.generateFileName();
     const filePath = path.join(
       this._filePath,
