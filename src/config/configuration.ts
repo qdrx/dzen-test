fa285b37d581e10ebe4eb599810f47b4abc9a56c
qdrx <qdrx@github.com>
2024-08-28 12:22:31 +0000

    chore: add jwt config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,11 @@
+import * as process from 'node:process';
+
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    ttl: process.env.JWT_TOKEN_TTL,
+  },
   database: {
     type: process.env.DATABASE_TYPE,
     host: process.env.DATABASE_HOST,
