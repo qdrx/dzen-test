799b61a3661154a7c0dd1062977e9fdaa478e482
qdrx <qdrx@github.com>
2024-08-29 18:54:58 +0000

    refactor: add allowed index types

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { ElasticsearchService } from '@nestjs/elasticsearch';
 import { QueryDslQueryContainer } from '@elastic/elasticsearch/lib/api/types';
+import { AllowedIndex } from './types/allowed-index.type';
 
 @Injectable()
 export class SearchService {
   constructor(private readonly elasticService: ElasticsearchService) {}
 
-  async search(index: string, query: QueryDslQueryContainer) {
+  async search(index: AllowedIndex, query: QueryDslQueryContainer) {
     return await this.elasticService.search({
       index,
       query,
     });
   }
 
-  async index<T>(index: string, document: T) {
+  async index<T>(index: AllowedIndex, document: T) {
     return await this.elasticService.index({
       index,
       document,
